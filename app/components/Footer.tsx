2ad55a9e1ea0b8d87bdb1ac363ae6bb8e699b425
CarolinaRTC <carolinacarles@byronsolutions.com>
2023-07-26 13:57:18 -0300

    Criando seção footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="bg-green-800 text-gray-100">
+      <h1>Resumo Seções</h1>
+    </footer>
+  );
 };
