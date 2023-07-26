b49d51accaf71e0cb2dafcc3c24cdfb63384cf62
CarolinaRTC <carolinacarles@byronsolutions.com>
2023-07-26 13:45:19 -0300

    Criando seção header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,5 +1,37 @@
 import React from "react";
 
 export const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="bg-orange-800 h-24 p-4 text-gray-100 font-bold drop-shadow-lg text-sm fixed w-full z-10">
+      <div className="w-1/2 flex justify-between">
+        <div>
+          <img
+            className="max-h-16"
+            src="img/logoantipesto.png"
+            alt="Logo AntiPesto"
+          />
+        </div>
+        <div className="w-1/2 max-h-16 flex items-center">
+          <nav>
+            <ul>
+              <li className="space-x-4">
+                <a className="hover:text-gray-400" href="">
+                  Home
+                </a>
+                <a className="hover:text-gray-400" href="">
+                  Empresa
+                </a>
+                <a className="hover:text-gray-400" href="">
+                  Serviços
+                </a>
+                <a className="hover:text-gray-400" href="">
+                  Contato
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
 };
