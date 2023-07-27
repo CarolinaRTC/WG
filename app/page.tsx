f52798ebe146b9ace648d18d87ffd094c8b14264
Hannah <hannah.clara.so@gmail.com>
2023-07-26 22:15:41 -0300

    Baixado o SWIPER e importado os  itens necessários para a montagem do carrossel de imagens.

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,18 @@
-import React from "react";
+"use client";
+import React, { useEffect, useLayoutEffect, useContext, createContext, forwardRef, useState, useRef } from "react";
 import { Header } from "./components/Header";
 import { Home } from "./components/Home";
 import { Sobre } from "./components/Sobre";
 import { Serviços } from "./components/Serviços";
 import { Contato } from "./components/Contato";
 import { Footer } from "./components/Footer";
+import { register } from 'swiper/element/bundle';
+
+register();
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 
 const page = () => {
   return (
