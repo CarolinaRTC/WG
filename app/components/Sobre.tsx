ba342718e61732eb8f0b6dc0c785c6e323a04509
Hannah <hannah.clara.so@gmail.com>
2023-07-27 09:56:52 -0300

    Melhoria dos textos presentes na seção 'Sobre a empresa'.

--- app/components/Sobre.tsx
+++ app/components/Sobre.tsx
@@ -5,17 +5,20 @@ export const Sobre = () => {
     <section className="text-xl m-10">
       <div className="text-center">
         <h1 className="bg-orange-800 text-4xl text-gray-100 font-bold p-3">SOBRE A EMPRESA</h1>
-        <p className="text-base font-montserrat pt-4">
-          Somos uma empresa com o objetivo de capturar criaturas prejudiciais a jardins e plantações, sendo nosso maior foco os coelhos.</p>
-          <p className="text-base text-center font-montserrat">Nossa equipe é excepcionalmente qualificada e utiliza apenas ferramentas autorais, tecnologias desenvolvidas pelas nossas próprias mãos e confiáveis mesmo nas situações mais difíceis.</p>
+        <p className="text-base font-montserrat pt-4 p-3">
+        Somos uma equipe de dedetizadores que tem como objetivo promover ambientes, especialmente jardins e hortas, livres de "parasitas". Em grande parte nosso trabalho é focado na retirada e realocação de coelhos, uma vez que seus hábitos costumam trazer diversos prejuízos para as plantações.</p>
+          <p className="text-base text-center font-montserrat p-3">Nosso time é altamente treinado para executar de forma rápida e eficiente, utilizando ferramentas e tecnologias autorais que tem sua confiabilidade comprovada através de testes em situações diversas e de anos de experiência no ramo.</p>
 
-          <p className="text-base text-center font-montserrat pb-2">Nossa empresa funciona bem em razão da forte base que seguimos: </p>
+          <p className="text-base text-center font-montserrat p-3 pb-2">Nossa empresa funciona bem em razão da forte base que seguimos: </p>
           <div className="flex justify-center gap-4">
-            <p className="bg-green-800 rounded-md text-xl text-gray-100 text-center font-montserrat font-italic font-bold py-3 px-2">SAFE</p>
-              <p className="bg-green-800 rounded-md text-xl text-gray-100 text-center font-montserrat font-italic font-bold py-3 px-2">SECURE</p>
-              <p className="bg-green-800 rounded-md text-xl text-gray-100 text-center font-montserrat font-italic font-bold py-3 px-2">HUMANE</p>
+            <p className="bg-green-800 rounded-md text-xl text-gray-100 text-center font-montserrat font-italic font-bold py-3 px-2">SEGURANÇA</p>
+              <p className="bg-green-800 rounded-md text-xl text-gray-100 text-center font-montserrat font-italic font-bold py-3 px-2">TRANSPARÊNCIA</p>
+              <p className="bg-green-800 rounded-md text-xl text-gray-100 text-center font-montserrat font-italic font-bold py-3 px-2">HUMANIDADE</p>
           </div>
-            <p className="text-base text-center font-montserrat pt-2 pb-4">Acreditamos que um serviço bem-feito é aquele em que todas as partes envolvidas saem ganhando, por isso a comunicação com os clientes é muito importante para nós.</p>
+            <p className="text-base font-montserrat pt-4 p-3">
+            A comunicação entre nosso time e o cliente é essencial, pois acreditamos que um serviço bem-feito só é possível quando executado com transparência, cuidado e excelência.</p>
+            <p className="text-base text-center font-montserrat p-3 pb-4">Nossa satisfação é poder contribuir para o bem-estar daqueles que contratam nossos serviços!</p>
+            
           <div className="flex items-center justify-center place-content-center">
             <img
             src="img/WGterno.png"
