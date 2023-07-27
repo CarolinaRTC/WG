8909db0141a63ac8c7661e6865f6a0e8486feff5
CarolinaRTC <carolinacarles@byronsolutions.com>
2023-07-27 13:59:24 -0300

    Formatando footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,8 +2,22 @@ import React from "react";
 
 export const Footer = () => {
   return (
-    <footer className="bg-green-800 text-gray-100">
-      <h1>Resumo Seções</h1>
+    <footer className="bg-green-800 text-gray-100 p-4">
+      <div className="flex flex-col justify-center gap-5">
+        <div>
+          <h1 className="font-bold">Nossa localização:</h1>
+          <p>Rua Caçando Coelhos, 123 - Vila dos Vegetais</p>
+          <p>Cidade dos Coelhos</p>
+        </div>
+        <div>
+          <h1 className="font-bold">Contato:</h1>
+          <p>(99) 99999-9999</p>
+          <p>antipesto@antipesto.com</p>
+        </div>
+        <div>
+          <h1 className="font-bold">Navegue:</h1>
+        </div>
+      </div>
     </footer>
   );
 };
